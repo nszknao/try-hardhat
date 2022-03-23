614d0c9e338934387a7aeab8fe20092a9a03e2fd
Naoto Nishizaka <nszk.nao@gmail.com>
2022-03-23 11:37:06 +0900

    add erc1155

--- contracts/ERC1155Token.sol
+++ contracts/ERC1155Token.sol
@@ -4,17 +4,5 @@ pragma solidity ^0.8.9;
 import "@openzeppelin/contracts/token/ERC1155/presets/ERC1155PresetMinterPauser.sol";
 
 contract ERC1155Token is ERC1155PresetMinterPauser {
-    uint256 public constant GOLD = 0;
-    uint256 public constant SILVER = 1;
-    uint256 public constant THORS_HAMMER = 2;
-    uint256 public constant SWORD = 3;
-    uint256 public constant SHIELD = 4;
-
-    constructor() ERC1155PresetMinterPauser("https://game.example/api/item/{id}.json") {
-        _mint(msg.sender, GOLD, 10**18, "");
-        _mint(msg.sender, SILVER, 10**27, "");
-        _mint(msg.sender, THORS_HAMMER, 1, "");
-        _mint(msg.sender, SWORD, 10**9, "");
-        _mint(msg.sender, SHIELD, 10**9, "");
-    }
+    constructor() ERC1155PresetMinterPauser("https://game.example/api/item/{id}.json") {}
 }
